56c0c52e6f077d6468399c04c2cb01fe9199c5fb
roshanroy7 <roshanpalem@gmail.com>
2026-05-08 16:06:06 +0100

    fixed port to 8000

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('ReMind API is running!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
